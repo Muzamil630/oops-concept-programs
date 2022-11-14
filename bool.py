61df82321de57482ea2b4b262a1c53b3cb8f2dd0
Muzamil630 <116160858+Muzamil630@users.noreply.github.com>
2022-11-14 22:06:08 +0530

    Add files via upload

---
+++ bool.py
@@ -0,0 +1,10 @@
+a=""
+print(bool(a))
+b=1
+print(bool(b))
+c=0
+print(bool(c))
+d="hello"
+print(bool(d))
+e=" "
+print(bool(e))
---
+++ fake.py
@@ -0,0 +1,15 @@
+# from faker import Faker
+# fakedata=Faker()
+# print(fakedata.name())
+# print(fakedata.email())
+# print(fakedata.password())
+# print(fakedata.address())
+
+# from faker import Faker
+# from openpyxl import Workbook
+# wb=Workbook()
+# ws=wb.active
+# fake_data=Faker()
+# for i in range(1,11):
+#     ws.cell(row=i,column=1).value=fake_data.name()
+# wb.save("3pmPython.xlsx")
